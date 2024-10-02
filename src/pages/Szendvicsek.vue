58b1deea5c6c30ed6cec29f8957732ce3f3dd853
WkRyQx <wolfeyes008@gmail.com>
2024-10-02 10:23:16 +0200

    zndics

--- src/pages/Szendvicsek.vue
+++ src/pages/Szendvicsek.vue
@@ -1,10 +1,10 @@
 <script setup>
 import Rightside from "../components/Rightside.vue";
 
-const szendvicsek = [
-  {nev: "Sonkás szendvics", ar: 570, kep: "sonkasszendvics.jpg"},
-  {nev: "Szalámis szendvics", ar: 570, kep: "szalamisszendvics.jpg"},
-  {nev: "Rántott húsos szendvics", ar: 800, kep: "rantotthusosszendvics.jpg"}
+const szendicsek = [
+  {nev: "Sonkás szendics", ar: 570, kep: "sonkasszendvics.jpg"},
+  {nev: "Szalámis szendics", ar: 570, kep: "szalamisszendvics.jpg"},
+  {nev: "Rántott húsos szedvics", ar: 800, kep: "rantotthusosszendvics.jpg"}
 ]
 
 </script>
