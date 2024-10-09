3d7a3293c720f1db5e0439c4e8e2114b32d4b64c
RzsPter <rozsa.pter2@gmail.com>
2024-10-09 12:04:06 +0200

    logo és anim
    logo hozzá adva és animáció kész

--- src/pages/Kezdes.vue
+++ src/pages/Kezdes.vue
@@ -3,11 +3,12 @@ import { RouterLink } from "vue-router";
 </script>
 
 <template>
-  <div class="h-screen"> 
-    <h1 class="w-screen h-1/3 flex justify-center items-end drop-shadow-2xl">Pollák Büfé</h1>
-    <div class="w-screen h-2/3 flex justify-center items-start pt-28"> 
+  <div class="h-screen">
+    <img class="logo w-screen absolute" src="/public/logo.png" alt="">
+    <h1 class="w-screen h-1/3 flex justify-center items-end drop-shadow-2xl move">Pollák Büfé</h1>
+    <div class="w-screen h-2/3 flex justify-center items-sta rt pt-28 move"> 
       <RouterLink to="/szendvicsek">
-        <button class="blink-button px-8 py-4 border-2 border-solid rounded-xl drop-shadow-2xl">
+        <button class="blink-button px-8 py-4 border-2 border-solid rounded-xl drop-shadow-2xl">  
           Kezdés
         </button> 
       </RouterLink>
@@ -41,15 +42,34 @@ p {
 
 /* Villogó gomb animáció */
 .blink-button {
-  animation: blink 2s infinite;
+  animation: blink 1s infinite;
+}
+
+.move {
+  animation: move 600s infinite;
+}
+
+.logo {
+  width: 120px;
+  height: 100px;
 }
 
 @keyframes blink {
-  0%, 100% {
-    background-color: rgb(61, 68, 135);
+  0%, 100%{
+    background-color: rgb(115, 126, 223);
   }
   50% {
-    background-color: rgb(124, 140, 223);
+    background-color: rgb(106, 77, 187);
   }
 }
+
+@keyframes move {
+  0%, 50%, 100% {
+    transform: translateY(0);
+  }
+  25%, 75% {
+    transform: translateY(2px);
+  }
+}
+
 </style>
