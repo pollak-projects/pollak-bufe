dc2c81d6fe6c4dd702cc518f68968bca508b852c
vargimatixx <124675743+vargimatixx@users.noreply.github.com>
2024-10-15 11:21:13 +0200

    utoljara csinaltam meg

--- src/pages/Extrak.vue
+++ src/pages/Extrak.vue
@@ -32,16 +32,29 @@ function Veglegesites() {
   getSelected();
   router.push("/kosar");
 }
+
+function getImageUrl() {
+  return new URL(`../assets/sajtoshambi.png`, import.meta.url);
+}
 </script>
 
 <template>
   <div class="bg"></div>
   <div class="w-full h-screen max-h-screen p-4">
-    <h1 class="text-[#554B4B] drop-shadow-lg text-5xl mb-20 ms-10">
+    <h1 class="text-[#554B4B] drop-shadow-lg text-6xl mb-20 ms-10">
       Sajtos hamburger
     </h1>
+    <div class="justify-center flex">
+      <img :src="getImageUrl()" alt="" class="w-4/5 h-4/5 mb-2" :class="{ active: isActive }" @click="toggleAnimation"/>
+    </div>
+    <div class="text-[#616161] drop-shadow-lg text-8xl mb-20 ms-10 justify-center flex">
+      <h1>1000 Ft</h1>
+    </div>
+    <div class="text-[#616161] drop-shadow-lg text-5xl mb-20 ms-10 text-center flex leirasfont">
+      Ínycsiklandó sajtos hamburger, másnapos bucival, és döghússal!
+    </div>
     <div class="h-[75%] flex gap-10">
-      <div class="h-[75%] flex flex-wrap gap-10 p-3 mb-13">
+      <div class="h-[50%] flex flex-wrap gap-10 p-5 mb-13 justify-center">
         <ExtraCard text="Ketchup" imgName="ketchup.png" v-model="extraModel" />
         <ExtraCard text="Mustár" imgName="must.png" v-model="extraModel" />
         <ExtraCard text="Majonéz" imgName="majo.png" v-model="extraModel" />
@@ -53,28 +66,16 @@ function Veglegesites() {
         />
       </div>
     </div>
-    <div class="absolute bottom-10 w-8/12 flex justify-between mx-6">
+    <div class="absolute bottom-10 w-11/12 flex justify-between mx-6">
       <button
-        class="border rounded-full border-black bg-[#d8dcff] p-3 px-12 text-[#554b4b] text-2xl"
+        class="border rounded-full border-black bg-[#d8dcff] p-3 px-16 text-[#554b4b] text-5xl"
         @click="Kosarba()"
       >
         Kosárba
       </button>
       <button
-        class="border rounded-full border-black bg-[#d8dcff] p-3 px-12 text-[#554b4b] text-2xl"
+        class="border rounded-full border-black bg-[#d8dcff] p-3 px-16 text-[#554b4b] text-5xl"
         @click="Veglegesites()"
-      >
-        Véglegesítés
-      </button>
-    </div>
-    <div class="absolute bottom-10 w-8/12 flex justify-between mx-6">
-      <button
-        class="border rounded-full border-black bg-[#d8dcff] p-3 px-12 text-[#554b4b] text-2xl"
-      >
-        Kosárba
-      </button>
-      <button
-        class="border rounded-full border-black bg-[#d8dcff] p-3 px-12 text-[#554b4b] text-2xl"
       >
         Véglegesítés
       </button>
