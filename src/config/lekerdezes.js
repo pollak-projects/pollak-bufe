c50d105b0b991b6d0541b89645b5441ff03568b4
Huszka Adrián Gábor <hadrian@pollak.hu>
2024-12-10 11:21:19 +0100

    Add console log for 'ital' in Rendeles_Cucc2 function

--- src/config/lekerdezes.js
+++ src/config/lekerdezes.js
@@ -225,6 +225,7 @@ export async function Rendeles_Cucc2(szunet, bankkartya) {
   let utolsoSorszam = await UtolsoSorszam();
   sorszam.sorszam = await UtolsoNapiSorszam();
   let ital = basketData.getAll("egyeb");
+  console.log(ital);
   basketData.append("sorszam", utolsoSorszam + 1);
   for (let i = 0; i < store2.kosar.length; i++) {
     const data = JSON.parse(JSON.stringify(store2.kosar[i].darab));
