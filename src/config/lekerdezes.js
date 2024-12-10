5e22d7ba8610ab3dfe5d83ab15c651405545955f
Huszka Adrián Gábor <hadrian@pollak.hu>
2024-12-10 12:04:39 +0100

    Fix condition check in Rendeles_Cucc2 to ensure proper handling of 'kosar' items

--- src/config/lekerdezes.js
+++ src/config/lekerdezes.js
@@ -228,7 +228,7 @@ export async function Rendeles_Cucc2(szunet, bankkartya) {
   basketData.append("sorszam", utolsoSorszam + 1);
   console.log(store.kosar);
   for (let i = 0; i < 4; i++) {
-    if (store2.kosar[i].darab != undefined) {
+    if (store2.kosar[i] != undefined) {
       const data = JSON.parse(JSON.stringify(store2.kosar[i].darab));
       const dataszosz = JSON.parse(JSON.stringify(store2.szoszok[i]));
       Rendeles_Cucc(
